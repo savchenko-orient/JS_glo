373f8b38a8739726106558460a4df84961d483a1
Oleksii Savchenko <savchenko.orient@gmail.com>
2020-11-23 18:01:07 +0200

    Второй урок готов

--- script/script.js
+++ script/script.js
@@ -1,13 +1,26 @@
 'use strict';
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission; 
-let period;
+let money = 4500; //Доход за месяц
+let income = "500"; //Дополнительный доход
+let addExpenses = "Taxi, Car, Girlfriend"; //Расходы
+let deposit = true;
+let mission = 15000; //Какую сумму хочу накопить
+let period = 5; //сколько месяцев
 
-alert("Hello World!");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log("Hello World!");
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев.");
+console.log('Цель заработать ' + mission + ' гривен.');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(','));
+
+let budgetDay;
+budgetDay = money / 30;
+console.log(budgetDay);
+
+
 
 
